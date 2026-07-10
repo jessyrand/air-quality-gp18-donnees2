522cea447cc47d1f686677667913c85d322889fd
harena-mihaja <mihaja2506@gmail.com>
2026-07-10 21:48:59 +0300

    feat: add test in main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
+from datetime import datetime
+
+from src.extract.fetch_history import export_to_csv, fetch_history_hourly_aqi
+from src.model.city import CITIES
+
+
 def main():
-    print("Hello from air-quality-gp18-donnees2!")
+    responses = fetch_history_hourly_aqi(
+        cities=CITIES,
+        start="2026-07-01",
+        end=datetime.now().strftime("%Y-%m-%d"),
+    )
+    export_to_csv(cities=CITIES, responses=responses)
 
 
 if __name__ == "__main__":
--- src/model/city.py
+++ src/model/city.py
@@ -11,3 +11,11 @@ class City:
         return math.isclose(self.latitude, lat, abs_tol=0.1) and math.isclose(
             self.longitude, long, abs_tol=0.1
         )
+
+CITIES = [
+    City("Paris",48.8534951,2.3483915),
+    City("Tokyo",35.6768601,139.7638947),
+    City("Sydney",-33.8698439,151.2082848),
+    City("New York", 40.7127281,-74.0060152),
+    City("Antananarivo", -18.9100122,47.5255809)
+]
